9b4c4abf45f1117beb6506235b860404ca7a6aeb
ianroller4 <ianroller4@gmail.com>
2026-03-08 17:07:12 -0500

    Game Over screen created.
    Created game over UI. Button works but does not restart game yet.

--- gameOver.js
+++ gameOver.js
@@ -1,7 +1,51 @@
 class GameOver {
-  constructor() {}
+  constructor() {
+    this.mouseWasPressed = false;
+  }
 
   update() {
-    text("Game Over", width / 2, height / 2);
+    this.drawText();
+    this.drawButton();
+    this.checkForButtonPress();
+  }
+
+  checkForButtonPress() {
+    if (mouseIsPressed && !this.mouseWasPressed) {
+      let x = mouseX;
+      let y = mouseY;
+      let xLB = width / 2 - width / 4;
+      let xRB = width / 2 + width / 4;
+      let yUB = (3 * height) / 4 - height / 8;
+      let yBB = (3 * height) / 4 + height / 8;
+
+      if (x >= xLB && x <= xRB && y <= yBB && y >= yUB) {
+        print("button clicked");
+      }
+    }
+    this.mouseWasPressed = mouseIsPressed;
+  }
+
+  drawText() {
+    push();
+    textSize(100);
+    fill("white");
+    textAlign(CENTER, CENTER);
+    text("Game Over", width / 2, height / 4);
+    pop();
+  }
+
+  drawButton() {
+    push();
+    rectMode(CENTER);
+    fill("white");
+    rect(width / 2, (3 * height) / 4, width / 4, height / 8);
+    pop();
+
+    push();
+    textSize(100);
+    fill("black");
+    textAlign(CENTER, CENTER);
+    text("Try Again", width / 2, (3 * height) / 4);
+    pop();
   }
 }
